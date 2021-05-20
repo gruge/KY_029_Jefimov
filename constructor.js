78ccd2f12fccf06a89467be1be194ad27114bd05
gruge <45673832+gruge@users.noreply.github.com>
2021-05-20 10:06:04 +0300

    Add files via upload

--- constructor.js
+++ constructor.js
@@ -1,3 +1,5 @@
+col=0;
+multiply=-1;
 class KY_029{
 	constructor(ctx,GND=false,RED=0,GREEN=0){
 		this.ctx=ctx;
@@ -5,6 +7,14 @@ class KY_029{
 		this.RED=RED;
 		this.GREEN=GREEN;
 		this.res="#000000";
+		
+		multiply+=1;
+		this.canvasWidth=document.getElementById("myCanvas").getAttribute("width");
+		this.canvasWidth=this.canvasWidth.substr(0,this.canvasWidth.length-2);
+		if(this.canvasWidth<300*(multiply+1)){col+=1; multiply=multiply-3;}
+		this.x=300*multiply;
+		this.y=150*col;
+		
 		this.exec();
 	}
 	exec(){
@@ -27,17 +37,17 @@ class KY_029{
 	draw(){
 		this.ctx.font = "15px Arial";
 		this.ctx.fillStyle = "#3d3d3d";
-		this.ctx.fillRect(110,10, 100, 80);
+		this.ctx.fillRect(this.x+110,this.y+10, 100, 80);
 		this.ctx.fillStyle = "#797b7d";
-		this.ctx.fillRect(80,20, 40, 10);
-		this.ctx.fillRect(80,45, 40, 10);
-		this.ctx.fillRect(80,70, 40, 10);
+		this.ctx.fillRect(this.x+80,this.y+20, 40, 10);
+		this.ctx.fillRect(this.x+80,this.y+45, 40, 10);
+		this.ctx.fillRect(this.x+80,this.y+70, 40, 10);
 		this.ctx.fillStyle = this.res;
-		this.ctx.fillRect(205,30, 10, 40);
-		this.ctx.fillRect(215,35, 30, 30);
+		this.ctx.fillRect(this.x+205,this.y+30, 10, 40);
+		this.ctx.fillRect(this.x+215,this.y+35, 30, 30);
 		this.ctx.fillStyle = "#FFFFFF";
-		this.ctx.fillText("KY-029",150,53);
-		this.ctx.fillText("-",130,30);
-		this.ctx.fillText("S",130,80);
+		this.ctx.fillText("KY-029",this.x+150,this.y+53);
+		this.ctx.fillText("-",this.x+130,this.y+30);
+		this.ctx.fillText("S",this.x+130,this.y+80);
 	}
 }
